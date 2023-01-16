cc5d177e46100c7820feb6386c447af3769270dd
NathanFacundo <miltonfacu96@gmail.com>
2023-01-16 14:47:40 -0600

    OC X Validar
    Nuevos Campos. (X's)

--- UanlSISM/UanlSISM/Controllers/OrdenCompraController.cs
+++ UanlSISM/UanlSISM/Controllers/OrdenCompraController.cs
@@ -55,6 +55,11 @@ namespace UanlSISM.Controllers
             public string Validar { get; set; }
             public string DescripcionOC { get; set; }
             public string Estatus_OC { get; set; }
+            
+            public string DescO { get; internal set; }
+            public int? CBarra { get; internal set; }
+            public int? Can { get; internal set; }
+            public int Id_Requicision { get; internal set; }
         }
 
         public ActionResult ObtenerRequisInicio()
@@ -206,7 +211,7 @@ namespace UanlSISM.Controllers
                              where a.claveOLD == FolioRequi.ToString()
                              select a
                             ).FirstOrDefault();
-                
+
                 var UsuarioOld = User.Identity.GetUserName();
                 var UsuarioOLD = (from a in db.Usuario
                                   where a.Usu_User == UsuarioOld
@@ -343,7 +348,7 @@ namespace UanlSISM.Controllers
                     {
                         DetalleOC.Total = (double?)decimal.Round((decimal)(DetalleOC.Cantidad * DetalleOC.PreUnit), 2);
                     }
-                    
+
                     ConBD2.SISM_DETALLE_OC.Add(DetalleOC);
                     ConBD2.SaveChanges();
 
@@ -352,7 +357,7 @@ namespace UanlSISM.Controllers
                         SubTotal_OC += Decimal.Round((decimal)(DetalleOC.Total), 2);
                         OC.Total_OC = (double?)SubTotal_OC;
                     }
-                    
+
 
 
                     ConBD2.SaveChanges();
@@ -360,8 +365,8 @@ namespace UanlSISM.Controllers
 
                 //Buscar en los items(medicamentos) del Detalle de la OC si alguno está pendiente (lo eliminaron a la hora de genera las OC)
                 var ItemPendiente_DetalleOC = (from a in ConBD2.SISM_DETALLE_OC
-                                    where a.ItemPendiente == true
-                                    select a
+                                               where a.ItemPendiente == true
+                                               select a
                                     ).ToList();
                 //Poner la OC como 'Parcial' ya que tiene items pendientes
                 if (ItemPendiente_DetalleOC.Count > 0)
@@ -374,7 +379,7 @@ namespace UanlSISM.Controllers
                 }
                 ConBD2.SaveChanges();
 
-                
+
                 //, INFO = FolioNuevo_Oc.Clave
                 return Json(new { MENSAJE = "Succe: Se generó la O.C" }, JsonRequestBehavior.AllowGet);
                 #endregion
@@ -652,7 +657,7 @@ namespace UanlSISM.Controllers
             }
         }
 
-        //------------------                            PARCIALES       ----------------------
+        //------------------                            PARCIALES      INICIO ----------------------
         [Authorize]
         public ActionResult OrdenCompraParcial()
         {
@@ -711,18 +716,24 @@ namespace UanlSISM.Controllers
                              where DetOC.ItemPendiente == true
                              select new
                              {
-                                 Folio = a.Clave,
-                                 Fecha = a.Fecha,
-                                 Usuario = a.UsuarioNuevo,
-                                 Cantidad = DetOC.CantidadItema_OC,
-                                 Precio = DetOC.PreUnit,
-                                 Descripcion = DetOC.Descripcion,
-                                 ClaveMed = DetOC.ClaveMedicamento,
-                                 PU = DetOC.PreUnit,
-                                 Total = DetOC.Total,
+                                 Folio =        a.Clave,
+                                 Fecha =        a.Fecha,
+                                 Usuario =      a.UsuarioNuevo,
+                                 Cantidad =     DetOC.CantidadItema_OC,
+                                 Precio =       DetOC.PreUnit,
+                                 Descripcion =  DetOC.Descripcion,
+                                 ClaveMed =     DetOC.ClaveMedicamento,
+                                 PU =           DetOC.PreUnit,
+                                 Total =        DetOC.Total,
                                  NombreProveedor = a.NombreProveedor,
-                                 a.Total_OC,
-                                 Pendiente = DetOC.ItemPendiente
+                                 a.             Total_OC,
+                                 Pendiente =    DetOC.ItemPendiente,
+
+                                 Requi =        a.Id_Requisicion,
+                                 DescO =        a.Descripcion,
+                                 CBarra =       DetOC.Id_CodigoBarrar,
+                                 Can =          DetOC.Cantidad,
+                                 Sustancia =    DetOC.Id_Sustencia
                              }).ToList();
 
                 var results1 = new List<ListCampos>();
@@ -741,7 +752,13 @@ namespace UanlSISM.Controllers
                         PrecioUnitario = (double)q.PU,
                         Total = (double)q.Total,
                         NombreProveedor = q.NombreProveedor,
-                        Total_OC = (double)q.Total_OC
+                        Total_OC = (double)q.Total_OC,
+
+                        Id_Requicision = (int)q.Requi,
+                        DescO = q.DescO,
+                        CBarra = q.CBarra,
+                        Can = q.Can,
+                        Id_Sustancia = (int)q.Sustancia
                     };
                     results1.Add(resultado);
                 }
@@ -754,5 +771,206 @@ namespace UanlSISM.Controllers
             }
         }
 
+        public JsonResult GenerarOC_Parcial(List<SISM_DET_REQUISICION> ListaOC, int FolioOC/*, string ProveedorReq*/)
+        {
+            var UsuarioRegistra = User.Identity.GetUserName();
+            var fecha = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+            var fechaDT = DateTime.Parse(fecha);
+            var ip_realiza = Request.UserHostAddress;
+            var IdUsuarioCifrado = User.Identity.GetUserId();
+            return null;
+            //try
+            //{
+            //    //PROVEEDOR de la Requi(Orden)
+            //    var Prov = (from a in db.Proveedor
+            //                where a.Prov_Nombre == ProveedorReq
+            //                select a
+            //                ).FirstOrDefault();
+            //    //REQUI que se hará Orden
+            //    var Requi = (from a in ConBD2.SISM_REQUISICION
+            //                 where a.claveOLD == FolioRequi.ToString()
+            //                 select a
+            //                ).FirstOrDefault();
+
+            //    var UsuarioOld = User.Identity.GetUserName();
+            //    var UsuarioOLD = (from a in db.Usuario
+            //                      where a.Usu_User == UsuarioOld
+            //                      select a).FirstOrDefault();
+
+            //    #region TODA LA ACCION
+            //    //CREAR ORDEN NUEVA a partir de una Requi
+            //    SISM_ORDEN_COMPRA OC = new SISM_ORDEN_COMPRA();
+            //    //OC.Clave = CLAVE.ToString();
+            //    OC.Id_Requisicion = Requi.Id_Requicision;
+            //    OC.Id_Proveedor = Prov.Id;
+            //    OC.Fecha = fechaDT;
+            //    OC.FechaMod = fechaDT;
+            //    OC.Forma_Pago = "";
+            //    OC.Folio = "";
+            //    OC.Status = false;
+            //    OC.UsuarioId = UsuarioOLD.UsuarioId;
+            //    OC.Cerrado = true;
+            //    OC.Cuadro = 1;
+            //    OC.UsuarioNuevo = UsuarioRegistra;
+            //    OC.IP_User = ip_realiza;
+            //    OC.NombreProveedor = Prov.Prov_Nombre;
+            //    OC.OC_PorValidar = "1"; //LA OC nace en 1, tiene que validarse para pasar a 2 (validada) despues tiene que Generarse la OC y pasa a 3, el Status cambiará a True
+            //    ConBD2.SISM_ORDEN_COMPRA.Add(OC);
+            //    ConBD2.SaveChanges();
+            //    //ACTUALIZAMOS LA REQUI EN SU COLUMNA 'EstatusOC' poniendo 1 ya que esa requi se hará O.C
+            //    ConBD2.Database.ExecuteSqlCommand("UPDATE SISM_REQUISICION SET EstatusOC = '1' WHERE Id_Requicision='" + Requi.Id_Requicision + "';");
+            //    //obtener la ultima 'clave' de la tabla OrdenCompra actualBD vieja) para que inserte un nuevo registro en la nueva BD CONSECUTIVO de la clave
+            //    var Clave = (from a in db.OrdenCompra
+            //                 select new
+            //                 {
+            //                     clave = a.clave
+            //                 }).OrderByDescending(u => u.clave).FirstOrDefault();
+            //    var AñoMes_Actual = string.Format("{0:yyMM}", fechaDT);
+            //    var UltimoConsecutivo_Clave = Convert.ToInt32(Clave.clave.Substring(4));
+            //    var ConsecutivoNuevo = ((UltimoConsecutivo_Clave) + 1);
+            //    var ConsecutivoNuevoTxt = "";
+            //    if (ConsecutivoNuevo < 100)
+            //    {
+            //        if (ConsecutivoNuevo < 9)
+            //        {
+            //            ConsecutivoNuevoTxt = "00" + ConsecutivoNuevo;
+            //        }
+            //        else
+            //        {
+            //            ConsecutivoNuevoTxt = "0" + ConsecutivoNuevo;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        ConsecutivoNuevoTxt = "" + ConsecutivoNuevo;
+            //    }
+            //    //Obtenemos la ultima O.C guardada(que es esta) para guardar su detalle
+            //    var IdOC = (from a in ConBD2.SISM_ORDEN_COMPRA
+            //                where a.UsuarioNuevo == UsuarioRegistra
+            //                where a.Fecha == fechaDT
+            //                select a).OrderByDescending(u => u.Id).FirstOrDefault();
+            //    //ACTUALIZAMOS LA CLAVE DE LA O'C 
+            //    ConBD2.Database.ExecuteSqlCommand("UPDATE SISM_ORDEN_COMPRA SET Clave = '" + AñoMes_Actual + ConsecutivoNuevoTxt + "' WHERE Id='" + IdOC.Id + "';");
+            //    //RECORREMOS  para guardar en la tabla DETALLE ORDEN
+            //    foreach (var item in ListaOC)
+            //    {
+            //        //CREAR EL DETALLE DE LA NUEVA ORDEN
+            //        SISM_DETALLE_OC DetalleOC = new SISM_DETALLE_OC();
+            //        //BUSCAMOS EN LA TABLA "CodigoBarras" el Id del Codigo de Barras, buscando por el Id de la Sustancia
+            //        var CodigoBarras = (from a in SISMFarmacia.CodigoBarras
+            //                            where a.Id_Sustancia == item.Id_Sustancia
+            //                            select a).FirstOrDefault();
+            //        //Obtenemos la info de SUSTANCIA de la tabla Detalle_Requi
+            //        var Sustancia = (from a in ConBD2.SISM_DET_REQUISICION
+            //                         where a.Clave == item.Clave
+            //                         select a).FirstOrDefault();
+            //        DetalleOC.Id_OrdenCompra = IdOC.Id;
+            //        DetalleOC.Id_CodigoBarrar = CodigoBarras.Id;
+            //        DetalleOC.Obsequio = 0;
+            //        DetalleOC.Status = false;
+            //        DetalleOC.Id_Sustencia = item.Id_Sustancia;
+            //        DetalleOC.Descripcion = Sustancia.Descripcion;
+            //        DetalleOC.ClaveMedicamento = Sustancia.Clave;
+            //        //1 quiere decir que el item está pendiente, 0 quiere decir que si se surtió el item
+            //        DetalleOC.ItemPendiente = item.CB_ELIMINAR;
+
+            //        //En DetalleOC.Cantidad se guarda la Cantidad de pzas que pide Almacen siempre
+            //        DetalleOC.Cantidad = item.Cantidad;
+
+            //        if (item.CANTIDAD_NUEVA > 0)
+            //        {
+            //            //Aqui se guardan las pzas que modificó Compras 
+            //            DetalleOC.CantidadOC = item.CANTIDAD_NUEVA;
+            //            //Se guarda la cantidad de items total (al modificarse la cantidad esa nueva cantidad pasa a ser la oficial)
+            //            DetalleOC.CantidadItema_OC = item.CANTIDAD_NUEVA;
+
+            //            if (item.CANTIDAD_NUEVA < item.Cantidad)
+            //            {
+            //                DetalleOC.ItemPendiente = true;
+            //                //OC.Estatus_OC = "OC Parcial";
+            //            }
+            //        }
+            //        else
+            //        {
+            //            //Si no se modificó la CANTIDAD_NUEVA, la cantidad por default pasa a ser la oficial
+            //            DetalleOC.CantidadItema_OC = item.Cantidad;
+            //        }
+
+            //        //Se valida si el PRECIO UNITARIO se modificó
+            //        if (item.PREUNIT_NUEVA > 0)
+            //        {
+            //            DetalleOC.PreUnit = item.PREUNIT_NUEVA;
+            //        }
+            //        else
+            //        {
+            //            DetalleOC.PreUnit = item.PrecioUnitario;
+            //        }
+            //        //Se valida si se ingresó una NUEVA CANTIDAD o un NUEVO PRECIO UNITARIO     $$TOTAL$$ 
+            //        if (item.CANTIDAD_NUEVA > 0 || item.PREUNIT_NUEVA > 0)
+            //        {
+            //            if (item.CANTIDAD_NUEVA > 0 && item.PREUNIT_NUEVA > 0)
+            //            {
+            //                DetalleOC.Total = (double?)decimal.Round((decimal)(item.CANTIDAD_NUEVA * item.PREUNIT_NUEVA), 2);
+            //            }
+            //            else
+            //            {
+            //                if (item.CANTIDAD_NUEVA > 0)
+            //                {
+            //                    DetalleOC.Total = (double?)decimal.Round((decimal)(item.CANTIDAD_NUEVA * item.PrecioUnitario), 2);
+            //                }
+            //                if (item.PREUNIT_NUEVA > 0)
+            //                {
+            //                    DetalleOC.Total = (double?)decimal.Round((decimal)(item.Cantidad * item.PREUNIT_NUEVA), 2);
+            //                }
+            //            }
+            //        }
+            //        else
+            //        {
+            //            DetalleOC.Total = (double?)decimal.Round((decimal)(DetalleOC.Cantidad * DetalleOC.PreUnit), 2);
+            //        }
+
+            //        ConBD2.SISM_DETALLE_OC.Add(DetalleOC);
+            //        ConBD2.SaveChanges();
+
+            //        if (item.CB_ELIMINAR == false)
+            //        {
+            //            SubTotal_OC += Decimal.Round((decimal)(DetalleOC.Total), 2);
+            //            OC.Total_OC = (double?)SubTotal_OC;
+            //        }
+
+
+
+            //        ConBD2.SaveChanges();
+            //    }
+
+            //    //Buscar en los items(medicamentos) del Detalle de la OC si alguno está pendiente (lo eliminaron a la hora de genera las OC)
+            //    var ItemPendiente_DetalleOC = (from a in ConBD2.SISM_DETALLE_OC
+            //                                   where a.ItemPendiente == true
+            //                                   select a
+            //                        ).ToList();
+            //    //Poner la OC como 'Parcial' ya que tiene items pendientes
+            //    if (ItemPendiente_DetalleOC.Count > 0)
+            //    {
+            //        OC.Estatus_OC = "OC Parcial";
+            //    }
+            //    else
+            //    {
+            //        OC.Estatus_OC = "OC Completa";
+            //    }
+            //    ConBD2.SaveChanges();
+
+
+            //    //, INFO = FolioNuevo_Oc.Clave
+            //    return Json(new { MENSAJE = "Succe: Se generó la O.C" }, JsonRequestBehavior.AllowGet);
+            //    #endregion
+            //}
+            //catch (Exception ex)
+            //{
+            //    return Json(new { MENSAJE = "Error: Error de sistema: " + ex.Message }, JsonRequestBehavior.AllowGet);
+            //}
+        }
+
+
+        //------------------                            PARCIALES      FIN ----------------------
     }
 }
