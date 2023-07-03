4e0b1c25dc5f5f964e34f1c54f591d52d46a9986
NathanFacundo <miltonfacu96@gmail.com>
2023-07-03 13:31:44 -0600

    .
    .

--- UanlSISM/UanlSISM/Controllers/SeguimientoController.cs
+++ UanlSISM/UanlSISM/Controllers/SeguimientoController.cs
@@ -11,6 +11,7 @@ namespace UanlSISM.Controllers
     {
         SERVMEDEntities8 db1 = new SERVMEDEntities8();               //Req, OC, Sustancia, Vale
         SERVMEDEntities5 db2 = new SERVMEDEntities5();               //Sustancia
+        SISM_SIST_MEDEntities ConBD = new SISM_SIST_MEDEntities();      //BD NUEVA PRODUCTIVA
 
         public ActionResult Trazabilidad()
         {
@@ -29,12 +30,14 @@ namespace UanlSISM.Controllers
             public double PrecioUnitario { get; set; }
             public double Subtotal { get; set; }
             public string Descripcion { get; set; }
+            public string ProveedorOC { get; internal set; }
         }
 
         public ActionResult ObtenerReporte(string FechaInicio, string FechaFin, string ClaveMed)
         {
             try
             {
+                #region Fechas
                 var fechaI = FechaInicio + " 00:00:00";
                 var fechaIn = DateTime.Parse(fechaI);
 
@@ -42,68 +45,76 @@ namespace UanlSISM.Controllers
                 var fechaFi = DateTime.Parse(fechaF);
 
                 var results1 = new List<Propiedades>();
+                #endregion
 
-                //Detalles Requis de la Clave enviada
-                var Lista_Requis = (from Req in db1.Requisicion
-                             join Dreq in db1.DetalleReq on Req.id equals Dreq.Id_Requisicion
-                             join Sus in db1.Sustancia on Dreq.Id_Sustancia equals Sus.Id
-                             where Req.Fecha >= fechaIn && Req.Fecha <= fechaFi
-                             where Sus.Clave == ClaveMed && Req.Id_Tipo == 2
-                             select new
-                             {
-                                 IdRequi = Req.id,
-                                 Clave = Sus.Clave,
-                                 FolioR = Req.clave,
-                                 FechaR = Req.Fecha,
-                                 CanR = Dreq.C_Solicitada,
-                                 Desc = Sus.descripcion_21,
-                                 IdSus = Sus.Id
-                             }).ToList();
+                //Buscar las partidas en las Requis con la clave ingresada      BD NUEVA
+                var PartidasRequis = (from R in ConBD.SISM_REQUISICION
+                                      join DR in ConBD.SISM_DET_REQUISICION on R.Id_Requicision equals DR.Id_Requicision
+                                      where R.Fecha >= fechaIn && R.Fecha <= fechaFi
+                                      where DR.Clave == ClaveMed
+                                      select new {
+                                          IdRequi = R.Id_Requicision,
+                                          Clave = DR.Clave,
+                                          FolioR = R.claveOLD,
+                                          FechaR = R.Fecha,
+                                          CanR = DR.Cantidad,
+                                          Desc = DR.Descripcion,
+                                          IdSus = DR.Id_Sustancia
+                                      }).ToList();
 
-                foreach (var q in Lista_Requis)
+                //
+                foreach (var a in PartidasRequis)
                 {
-                    var PartidaOC = (from OC in db1.OrdenCompra 
-                                    join Doc in db1.DetalleOC on OC.Id equals Doc.Id_OrdenCompra
-                                    where OC.Id_Requisicion == q.IdRequi
-                                    where Doc.Id_Sustancia == q.IdSus
-                                    select new
-                                    {
-                                        FolioOC = OC.clave,
-                                        FechaOC = OC.Fecha,
-                                        CanOC = Doc.Cantidad,
-                                        PU = Doc.PreUnit,
-                                    }).FirstOrDefault();
+                    //Recorremos la lista de partidas encontradas con esos filtros para vi si ya se encuentran o no en OC
+                    var PartidaEnOC = (from O in ConBD.SISM_ORDEN_COMPRA
+                                       join DO in ConBD.SISM_DETALLE_OC on O.Id equals DO.Id_OrdenCompra
+                                       join R in ConBD.SISM_REQUISICION on O.Id_Requisicion equals R.Id_Requicision
+                                       join DR in ConBD.SISM_DET_REQUISICION on R.Id_Requicision equals DR.Id_Requicision
+                                       where O.Id_Requisicion == a.IdRequi && DO.Id_Sustencia == a.IdSus
+                                       where DR.Id_Sustancia == a.IdSus
+                                        select new
+                                        {
+                                            FolioOC = O.Clave,
+                                            FechaOC = O.Fecha_HacerOC,
+                                            CanOC = DO.Cantidad,
+                                            PU = DO.PreUnit,
+                                            Prov = O.NombreProveedor,
+                                            Clave = DO.ClaveMedicamento,
+                                            FolioR = R.claveOLD,
+                                            FechaR = R.Fecha,
+                                            CanR = DR.Cantidad,
+                                            Desc = DO.Descripcion
+                                        }).FirstOrDefault();
 
-                    if (PartidaOC != null)
+                    if (PartidaEnOC != null)//Si ya se encuentra en alguna OC la agregamos a la lista con la info de la OC
                     {
                         var Partidas = new Propiedades
                         {
-                            FolioOC = PartidaOC.FolioOC,
-                            FechaOC = string.Format("{0:d/M/yyyy hh:mm tt}", PartidaOC.FechaOC),
-                            CanOC = PartidaOC.CanOC,
-                            PrecioUnitario = PartidaOC.PU,
-                            //
-                            //Clave = PartidaOC.Clave,
-                            //FolioRequisicion = PartidaOC.FolioR,
-                            //FechaRequisicion = string.Format("{0:d/M/yyyy hh:mm tt}", PartidaOC.FechaR),
-                            //CanReq = PartidaOC.CanR,
-                            //Descripcion = PartidaOC.Desc
+                            FolioOC = PartidaEnOC.FolioOC,
+                            FechaOC = string.Format("{0:d/M/yyyy hh:mm tt}", PartidaEnOC.FechaOC),
+                            CanOC = (int)PartidaEnOC.CanOC,
+                            PrecioUnitario = (double)PartidaEnOC.PU,
+                            ProveedorOC = PartidaEnOC.Prov,
+                            Clave = PartidaEnOC.Clave,
+                            FolioRequisicion = PartidaEnOC.FolioR,
+                            FechaRequisicion = string.Format("{0:d/M/yyyy hh:mm tt}", PartidaEnOC.FechaR),
+                            CanReq = (int)PartidaEnOC.CanR,
+                            Descripcion = PartidaEnOC.Desc
                         };
                         results1.Add(Partidas);
                     }
-                }
-
-                foreach (var item in Lista_Requis)
-                {
-                    var Partidas = new Propiedades
+                    else//Si no tiene OC aún así agregamos la partida a la lista con la info solo de la Requi
                     {
-                        Clave = item.Clave,
-                        FolioRequisicion = item.FolioR,
-                        FechaRequisicion = string.Format("{0:d/M/yyyy hh:mm tt}", item.FechaR),
-                        CanReq = item.CanR,
-                        Descripcion = item.Desc
-                    };
-                    results1.Add(Partidas);
+                        var Partidas = new Propiedades
+                        {
+                            Clave = a.Clave,
+                            FolioRequisicion = a.FolioR,
+                            FechaRequisicion = string.Format("{0:d/M/yyyy hh:mm tt}", a.FechaR),
+                            CanReq = (int)a.CanR,
+                            Descripcion = a.Desc
+                        };
+                        results1.Add(Partidas);
+                    }
                 }
 
                 return Json(new { MENSAJE = "FOUND", REP = results1 }, JsonRequestBehavior.AllowGet);
